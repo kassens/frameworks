9c878aaa326e547611b3296f1c71d0095cac509c
Fabian Beiner <mail@fabian-beiner.de>
2010-12-20 23:27:24 +0100

    Added a few more libraries.

--- global.js
+++ global.js
@@ -1,18 +1,25 @@
 var icons = {
-	'MooTools Core': 'mootools.png',
-	'MooTools More': 'mootools-more.png',
-	// 'Base2',
+	//'Base2': '',
+	'Clientcide Libraries': 'clientcide-libraries.png',
 	'Dojo': 'dojo.png',
-	'Ext JS': 'extjs.png',
+	'Ext JS': 'ext-js.png',
+	'Glow': 'glow.png',
+	'jQuery UI': 'jquery-ui.png',
 	'jQuery': 'jquery.png',
-	'jQuery UI': 'jqueryui.png',
 	'MochiKit': 'mochikit.png',
+	//'MooTools A.R.T.': '',
+	'MooTools Core': 'mootools.png',
+	'MooTools More': 'mootools.png',
+	'Processing.js': 'processing-js.png',
 	'Prototype': 'prototype.png',
-	'Script.aculo.us': 'scriptaculous.png',
-	'YUI': 'yui.png',
 	'Raphaël': 'raphael.png',
-	'Scripty2': 'scriptaculous.png'
-};
+	'Rico': 'rico.png',
+	'RightJS': 'rightjs.png',
+	'Script.aculo.us': 'scriptaculous.png',
+	//'Scripty2': '',
+	'YUI': 'yui.png',
+	'ZK': 'zk.png'
+}
 
 chrome.extension.onRequest.addListener(function(frameworks, sender){
 	if (frameworks.length == 0) return;
--- inject.js
+++ inject.js
@@ -2,22 +2,29 @@
 // No prototype usage here, it's in the page context!
 (function(){
 	var frameworks = {
-		'MooTools.version': 'MooTools Core',
-		'MooTools.More.version': 'MooTools More',
+		'$.ui.version': 'jQuery UI',
+		'ART.version': 'MooTools A.R.T.',
 		'base2.version': 'Base2',
+		'Clientcide.version': 'Clientcide Libraries',
 		'dojo.version': 'Dojo',
 		'Ext.version': 'Ext JS',
+		'glow.VERSION': 'Glow',
 		'jQuery.fn.jquery': 'jQuery',
-		'$.ui.version': 'jQuery UI',
 		'MochiKit.MochiKit.VERSION': 'MochiKit',
+		'MooTools.More.version': 'MooTools More',
+		'MooTools.version': 'MooTools Core',
+		'Processing.version': 'Processing.js',
 		'Prototype.Version': 'Prototype',
+		'Raphael.version': 'Raphaël',
+		'Rico.Version': 'Rico',
+		'RightJS.version': 'RightJS',
+		'S2.Version': 'Scripty2',
 		'Scriptaculous.Version': 'Script.aculo.us',
 		'YAHOO.VERSION': 'YUI', // YUI 2
 		'YUI.version': 'YUI', // YUI 3
-		'Raphael.version': 'Raphaël',
-		'S2.Version': 'Scripty2'
+		'zk.version': 'ZK'
 	};
-	
+
 	var data = [];
 	for (ns in frameworks){
 		if (!frameworks.hasOwnProperty(ns)) continue;
@@ -25,7 +32,7 @@
 		for (var i = 0, idents = ns.split('.'); i < idents.length; i++) version = version && version[idents[i]];
 		if (version) data.push('{"name":"' + frameworks[ns] + '","version":"' + version + '"}');
 	}
-	
+
 	var messageElement = document.createElement('div');
 	messageElement.textContent = '[' + data.join(',') + ']';
 	messageElement.id = 'frameworks-data';
