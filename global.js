7258cc56375589175847f97c3d0ddddf9be54904
Fabian Beiner <mail@fabian-beiner.de>
2010-12-25 17:33:12 +0100

    Added a few more frameworks.

--- global.js
+++ global.js
@@ -1,23 +1,32 @@
 var icons = {
+	//'ActiveJS': 'ActiveSupport',
 	//'Base2': '',
 	'Clientcide Libraries': 'clientcide-libraries.png',
+	'Crafty': 'crafty.png',
+	'DHTMLX': 'dhtmlx.png',
 	'Dojo': 'dojo.png',
 	'Ext JS': 'ext-js.png',
 	'Glow': 'glow.png',
-	'jQuery UI': 'jquery-ui.png',
+	'JavaScriptMVC': 'javascriptmvc.png',
 	'jQuery': 'jquery.png',
+	'jQuery UI': 'jquery-ui.png',
+	'Midori': 'midori.png',
 	'MochiKit': 'mochikit.png',
 	//'MooTools A.R.T.': '',
 	'MooTools Core': 'mootools.png',
 	'MooTools More': 'mootools.png',
 	'Processing.js': 'processing-js.png',
 	'Prototype': 'prototype.png',
+	'Qooxdoo': 'qooxdoo.png',
 	'Raphaël': 'raphael.png',
 	'Rico': 'rico.png',
 	'RightJS': 'rightjs.png',
 	'Script.aculo.us': 'scriptaculous.png',
 	//'Scripty2': '',
+	//'SproutCore': 'SC.isReady',
+	//'Spry': 'Spry.$',
 	'YUI': 'yui.png',
+	//'Zepto': 'Zepto',
 	'ZK': 'zk.png'
 }
 
@@ -28,7 +37,11 @@ chrome.extension.onRequest.addListener(function(frameworks, sender){
 	var tabId = sender.tab.id;
 
 	var title = frameworks.map(function(framework){
-		return framework.name + ' (' + framework.version + ')';
+		if (framework.version) {
+			return framework.name;
+		} else {
+			return framework.name + ' (' + framework.version + ')';
+		}
 	}).join('\n');
 	chrome.pageAction.setTitle({tabId: tabId, title: title});
 
