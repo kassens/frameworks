e461d0b7ffe9f1b0452347fb99d2d1833846c695
Jan Kassens <jan@kassens.net>
2012-09-08 23:00:45 +0100

    fix bug with manifest.json v2

--- Makefile
+++ Makefile
@@ -1,9 +1,10 @@
 JS_FILES := $(wildcard *.js)
 HTML_FILES := $(wildcard *.html)
+CSS_FILES := $(wildcard *.css)
 IMAGES := images/logo16.png images/logo48.png images/logo128.png
 
 all: extension.zip
 
-extension.zip: manifest.json LICENSE ${JS_FILES} ${HTML_FILES} ${IMAGES}
+extension.zip: manifest.json LICENSE ${JS_FILES} ${HTML_FILES} ${CSS_FILES} ${IMAGES}
 	rm -f $@
 	zip $@ $^
---
+++ popup.js
@@ -0,0 +1,16 @@
+function init(){
+	var hash = document.location.hash.substr(1);
+	var frameworks = JSON.parse(decodeURIComponent(hash));
+	var ul = document.getElementById('frameworks');
+	frameworks.forEach(function(framework){
+		var li = document.createElement('li');
+		li.textContent = framework.name;
+		if (framework.version) {
+			var version = document.createElement('span');
+			version.textContent = ' (' + framework.version + ')';
+			li.appendChild(version);
+		}
+		ul.appendChild(li);
+	});
+}
+document.addEventListener('DOMContentLoaded', init, false);
